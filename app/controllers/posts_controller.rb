246871a0d47d581d05fd9a77110bfabfa993cc9f
Chester Asuncion <asuncionmcv@gmail.com>
2021-12-22 16:44:10 +0800

    adding functionality to view only member posts in their profile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_member!, except: %i[index show]
 
   # GET /posts or /posts.json
   def index
     @posts = Post.all
-    @post = Post.new
   end
 
   # GET /posts/1 or /posts/1.json
@@ -22,11 +22,11 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_member.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
