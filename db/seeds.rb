744c66ccb399969eb515c6e70f228b25dcd7734c
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-15 23:29:50 +0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# create default user
+Member.create(
+  username: 'Constant Presence',
+  email: 'constant@email.com',
+  password: 'constant@email.com'
+)
+
+# create other members
+15.times do
+  username = Faker::Name.unique.name
+  Member.create(
+    username: username, 
+    email: Faker::Internet.unique.email(name: username),
+    password: 'random_password'
+  )
+end
+
+# create posts
+Member.all.each do |member|
+  rand(1..2).times do
+    member.posts.create!(
+      title: Faker::Company.bs,
+      content: Faker::Company.catch_phrase
+    )
+  end
+end
